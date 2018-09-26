2cfb37aff13cb87d5445a4761e38e5510090ecf6
Marcin Wolf <wolflint@users.noreply.github.com>
2018-09-26 20:18:52 +0100

    day 1 commit

---
+++ Scripts/Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+const SPEED = 750
+const GRAVITY = 3600
+const UP = Vector2(0, -1)
+const JUMP_SPEED = -1500
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	fall(delta)
+	run()
+	jump()
+	move_and_slide(motion, UP)
+
+
+
+func fall(delta):
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY * delta
+
+
+
+func run():
+	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		motion.x = SPEED
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("run")
+	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+		motion.x = -SPEED
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("run")
+	else:
+		motion.x = 0
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("idle")
+
+
+
+func jump():
+	if is_on_floor() and Input.is_action_pressed("ui_up"):
+		motion.y = JUMP_SPEED
